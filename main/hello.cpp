8b2cf3d7d026c7ce5c26a45aef53a7d3f968291c
Vinayak U <114764937+vinayakyou@users.noreply.github.com>
2025-03-11 20:06:50 +0530

    Updated hello.cpp again

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "subaru and pagani  \n";
+  cout << "subaru and pagani reloaded \n";
   return 0;
 }
